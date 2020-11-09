4a517690108321e959475811f3e1fb1fd1f734db
mta630 <Michael.t.albert89@gmail.com>
2020-11-09 11:57:01 -0600

    Created header bar with working personal links

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,9 +3,23 @@ import styles from "../modules/app.module.css";
 import { Link } from "react-router-dom";
 
 const App = (props) => {
+
+
   return (
     <div className={styles.appContainer}>
-      <div className={styles.title}>Michael Albert's Portfolio</div>
+      <div className={styles.title}>
+        <div className={styles.titleName}>Michael Albert</div>
+        <div className={styles.personalLinks}>
+          <div className={styles.individualLink}><a href="https://www.linkedin.com/in/mta630/" target="_blank"><i className="fab fa-linkedin-in"></i></a></div>
+          <div className={styles.individualLink}><a href="https://www.instagram.com/shmooby89/" target="_blank"><i className="fab fa-instagram"></i></a></div>
+          <div className={styles.individualLink}><a href="https://www.github.com/mta630/" target="_blank"><i className="fab fa-github"></i></a></div>
+        </div>
+        <div className={styles.siteLinks}>
+          <div className={styles.siteLink}>About Me</div>
+          <div className={styles.siteLink}>Projects</div>
+          <div className={styles.siteLink}>Contact Me</div>
+        </div>
+      </div>
     </div>
   );
 };
